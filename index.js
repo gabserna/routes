7b1c53f2e7ef9e431d180a885858d7edb4f72dcf
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-05-10 09:56:16 -0600

    very first

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/*pp.*', (req, res) => {
+    res.send('good');
+});
+
+app.get('/users/:userId/color/:color', (req, res) => {
+    let id = req.params.userId;
+    console.log(req.params);
+    console.log(id);
+    res.send(`your user id is ${id} ${req.params.color} `)
+})
+
+app.route('/route')
+.get((req, res) => {
+    res.send('from a get request')
+})
+.post((req, res) => {
+    let count = req.body.count;
+    let color = req.body.color;
+    console.log(req.body);
+    res.send('from a post request ' + count + ' color ' + color)
+})
+
+app.listen(port, () => {
+    console.log(`Server is up, listening on port: ${port}`);
+})
---
+++ readme.md
@@ -0,0 +1,11 @@
+* instalar nodemon
+npm install --save-dev nodemon
+
+git init
+echo 'node_modules/' >.gitignore
+npm init -y
+
+https://www.postman.com/downloads/
+curl -o- "https://dl-cli.pstmn.io/install/osx_arm64.sh" | sh
+
+# routes
